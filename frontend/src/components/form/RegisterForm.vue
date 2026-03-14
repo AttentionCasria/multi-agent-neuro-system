84c4612c5f02247f236b657811047c02ddb336d4
Betta_Fish <zxbmmmmmmmmm@qq.com>
2026-03-14 16:37:04 +0800

    注册时无需上传头像

--- frontend/src/components/form/RegisterForm.vue
+++ frontend/src/components/form/RegisterForm.vue
@@ -1,13 +1,11 @@
 <script setup>
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
-import AvatarUpload from '../AvatarUpload.vue'
 import { registerAPI, loginAPI } from '@/api/user'
 import { useUserStore } from '@/stores/user'
 
 const form$ = ref(null)
 const router = useRouter()
-const isUploadTipShow = ref(false)
 const userStore = useUserStore()
 
 const registerFormData = ref({
@@ -15,12 +13,6 @@ const registerFormData = ref({
   password: '',
   password_confirmation: '',
 })
-const image = ref('')
-
-function handleAvatarUploadSuccess(url) {
-  image.value = url
-  isUploadTipShow.value = false
-}
 
 async function handleRegister() {
   // 表单校验
@@ -29,80 +21,63 @@ async function handleRegister() {
     return
   }
 
-  // 图像校验
-  if (image.value === null || image.value === '') {
-    alert('请上传头像')
-    isUploadTipShow.value = true
-    return
-  }
-
   // 组装form
   const registerAPIData = {
     name: registerFormData.value.name,
     password: registerFormData.value.password,
-    image: image.value
   }
 
-  const res = await registerAPI(registerAPIData)
-
-  userStore.name = res.data.name
-  userStore.token = res.data.token
-  userStore.image = image.value  // 由于接口没有返回头像地址，直接用上传的地址
-
-  // 注册后直接登录
   try {
-    const res = await loginAPI({
-      name: registerFormData.value.name,
-      password: registerFormData.value.password,
-    })
-    if (res.code === 1) {
-      userStore.name = res.data.name
-      userStore.image = res.data.image
-      userStore.token = res.data.token
+    const res = await registerAPI(registerAPIData)
+    userStore.name = res.data.name
+    userStore.token = res.data.token
 
-      // 跳转到对话
-      router.replace('/')
+    // 注册后直接登录
+    try {
+      const res = await loginAPI({
+        name: registerFormData.value.name,
+        password: registerFormData.value.password,
+      })
+      if (res.code === 1) {
+        userStore.name = res.data.name
+        userStore.image = res.data.image
+        userStore.token = res.data.token
+
+        // 跳转到对话
+        router.replace('/')
+      }
+    } catch (err) {
+      if (err?.code === 0) {
+        alert('密码错误')
+      } else {
+        alert(err?.msg || '登录失败，请稍后再试')
+      }
     }
   } catch (err) {
-    if (err?.code === 0) {
-      alert('密码错误')
-    } else {
-      alert(err?.msg || '登录失败，请稍后再试')
-    }
+    alert(err?.msg || '注册失败，请稍后再试')
+    return
   }
 }
 </script>
 
 <template>
-  <Vueform
-    validate-on="change"
-    :display-errors="false"
-    size="lg"
-    v-model="registerFormData"
-    ref="form$">
+  <Vueform validate-on="change" :display-errors="false" size="lg" v-model="registerFormData" ref="form$">
     <StaticElement name="head" :loading="true">
       <h2>注册</h2>
     </StaticElement>
 
-    <TextElement
-      name="name"
-      size="lg"
-      placeholder="请输入用户名"
-      rules="required|min:3|max:20"
-      :debounce="300"
-      :messages="{
-        required: '用户名不能为空',
-        min: '用户名至少为3个字符',
-        max: '用户名至多为20个字符',
-      }">
+    <TextElement name="name" size="lg" placeholder="请输入用户名" rules="required|min:3|max:20" :debounce="300" :messages="{
+      required: '用户名不能为空',
+      min: '用户名至少为3个字符',
+      max: '用户名至多为20个字符',
+    }">
       <template #addon-before>
         <UserSVG size="20" color="#64748b"></UserSVG>
       </template>
     </TextElement>
 
     <TextElement name="password" input-type="password" placeholder="请输入密码" rules="required|min:6|confirmed"
-      :debounce="300"
-      :messages="{
+      :debounce="300" :messages="{
         required: '密码不能为空',
         min: '密码至少为6个字符',
         confirmed: '两次密码不一致',
@@ -113,21 +88,13 @@ async function handleRegister() {
     </TextElement>
 
     <TextElement name="password_confirmation" input-type="password" placeholder="请再次确认密码" rules="required"
-      :debounce="300"
-      :messages="{
+      :debounce="300" :messages="{
         required: '确认密码不能为空',
       }">
       <template #addon-before>
         <PasswordSVG size="24" color="#64748b"></PasswordSVG>
       </template>
     </TextElement>
-
-    <!-- 图片上传 -->
-    <StaticElement name="avatar-upload">
-      <div class="avatar-upload">
-        <AvatarUpload :showTip="isUploadTipShow" @uploaded="url => handleAvatarUploadSuccess(url)" />
-      </div>
-    </StaticElement>
 
     <ButtonElement name="submit" @click="handleRegister" full> 注册 </ButtonElement>
   </Vueform>
@@ -137,12 +104,6 @@ async function handleRegister() {
 h2 {
   color: #1e293b;
   margin-bottom: 1.5rem;
-}
-
-.avatar-upload {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
 }
 
 .preview img {
